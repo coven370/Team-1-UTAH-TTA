4ff41708fa4f65a5d1de81c716d8104dec65046d
Kode Creer <kode.creer@gmail.com>
2025-03-19 14:55:23 -0600

    made this fit better in requirments for citing sources

--- src/web/rag.py
+++ src/web/rag.py
@@ -2,4 +2,4 @@
 
 #TODO Perform Vector Search here thats a wrapper around your code. 
 def perform_search(prompts, top_k):
-    return ['No context' for _ in range(len(prompts))]
+    return ['No context' for _ in range(len(prompts))], ['No source' for _ in range(len(prompts))]
