e5a45d77e0c89c4369eca884d17470459378f8fc
coven370 <Coven370@gmail.com>
2025-04-08 23:21:49 -0600

    CommonHandler.js update

--- src/ui/src/servicehandlers/CommonHandler.js
+++ src/ui/src/servicehandlers/CommonHandler.js
@@ -3,150 +3,136 @@ import axios from 'axios';
 export class CommonAPIService {
   constructor() {
     this.api_url = process.env.VUE_APP_API_URL;
-    console.log(this.api_url);
+    console.log(this.api_url)
     this.loggedIn = false;
   }
 
   newFileCall(url, files, router) {
     const fullUrl = this.api_url + url;
+    axios.defaults.headers.common.Authorization = localStorage.getItem('jwtToken');
     return axios({
       method: 'get',
       url: fullUrl,
       responseType: 'blob',
-      headers: {
-        'Authorization': localStorage.getItem('jwtToken'),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
     })
-        .then((response) => {
-          return response;
-        })
-        .catch((error) => {
-          if (error.response && error.response.status === 401) {
-            router.push({ path: '/login' });
-          }
+    .then((response) => {
+      return response;
+    })
+    .catch((error) => {
+      if (error.response.status === 401) {
+        router.push({
+          path: '/login'
         });
+      }
+    });
   }
 
-  // Main login method for the code
-  login(loginPayload, router, store, errors, jump = true) {
-    return axios.post(`${this.api_url}/api/auth/login`, loginPayload, {
-      headers: {
-        'Authorization': localStorage.getItem('jwtToken'),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-        .then((response) => {
-          localStorage.setItem('jwtToken', response.data.token);
-          store.dispatch('ADD_USER', response.data.user);
-          store.dispatch('SET_LOGGED_IN', true);
-          return response.data;
-        })
-        .catch((e) => {
-          errors.push(e);
-          if (e.response && e.response.status === 401 && jump) {
-            router.push({ path: '/login' });
+  // main login method for the code
+  login(login, router, store, errors, jump = true) {
+    axios.defaults.headers.common.Authorization = localStorage.getItem('jwtToken');
+    return axios.post(`${this.api_url}/api/auth/login`, login)
+      .then((response) => {
+        localStorage.setItem('jwtToken' + '', response.data.token);
+        store.dispatch('ADD_USER', response.data.user);
+        store.dispatch('SET_LOGGED_IN', true)
+        return response.data;
+      })
+      .catch((e) => {
+        errors.push(e);
+        if (e.response.status === 401) {
+          if (jump) {
+            router.push({
+              path: '/login',
+            });
           }
-          return { success: false, msg: 'Invalid username and/or password. Please try again' };
-        });
+        }
+        return {success: false, msg: 'Invalid username and/or password. Please try again'};
+      });
   }
 
   postCall(url, parameters, router) {
+    axios.defaults.headers.common.Authorization = localStorage.getItem('jwtToken');
     const fullUrl = this.api_url + url;
-    return axios.post(fullUrl, parameters, {
-      headers: {
-        'Authorization': localStorage.getItem('jwtToken'),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-        .then((response) => {
-          return response.data;
-        })
-        .catch((error) => {
-          if (error.response && error.response.status === 401) {
-            router.push({ path: '/login' });
-          }
-          throw error;
-        });
+    return axios.post(fullUrl, parameters)
+      .then((response) => {
+        // console.debug('postCall response', response);
+        return response.data
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          router.push({
+            path: '/login',
+          });
+        }
+        throw error
+      });
   }
 
   fileCall(url, files, router) {
     const fullUrl = this.api_url + url;
-    // For file uploads, we typically use multipart/form-data
-    return axios.post(fullUrl, files, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-        'Authorization': localStorage.getItem('jwtToken'),
-        'Accept': 'application/json'
-      }
+    return axios.post(fullUrl, files,
+      {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'Authorization': localStorage.getItem('jwtToken'),
+        }
+      })
+      .then((response) => {
+      return response
+      // return response.data
     })
-        .then((response) => {
-          return response;
-        })
-        .catch((error) => {
-          if (error.response && error.response.status === 401) {
-            router.push({ path: '/login' });
-          }
-          throw error;
-        });
+      .catch((error) => {
+        if (error.response.status === 401) {
+          router.push({
+            path: '/login',
+          });
+        }
+        throw error
+      });
   }
 
   deleteCall(url, parameters, router) {
+    axios.defaults.headers.common.Authorization = localStorage.getItem('jwtToken');
     const fullUrl = this.api_url + url;
-    return axios.delete(fullUrl, {
-      headers: {
-        'Authorization': localStorage.getItem('jwtToken'),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      data: parameters // Use the data property to pass request body with DELETE
-    })
-        .then((response) => response.data)
-        .catch((error) => {
-          if (error.response && error.response.status === 401) {
-            router.push({ path: '/login' });
-          }
-        });
+    return axios.delete(fullUrl, parameters)
+      .then(response => response.data)
+      .catch((error) => {
+        if (error.response.status === 401) {
+          router.push({
+            path: '/login'
+          });
+        }
+      });
   }
 
   putCall(url, parameters, router) {
+    axios.defaults.headers.common.Authorization = localStorage.getItem('jwtToken');
     const fullUrl = this.api_url + url;
-    return axios.put(fullUrl, parameters, {
-      headers: {
-        'Authorization': localStorage.getItem('jwtToken'),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    })
-        .then((response) => response.data)
-        .catch((error) => {
-          if (error.response && error.response.status === 401) {
-            router.push({ path: '/login' });
-          }
-          throw error;
-        });
+    return axios.put(fullUrl, parameters)
+      .then(response => response.data)
+      .catch((error) => {
+        if (error.response.status === 401) {
+          router.push({
+            path: '/login'
+          });
+        }
+        throw error;
+      });
   }
 
   getCall(url, parameters, router) {
+    axios.defaults.headers.common.Authorization = localStorage.getItem('jwtToken');
     const fullUrl = this.api_url + url;
-    return axios.get(fullUrl, {
-      headers: {
-        'Authorization': localStorage.getItem('jwtToken'),
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      params: parameters // Pass query parameters through the "params" property
-    })
-        .then((response) => response.data)
-        .catch((error) => {
-          if (error.response && error.response.status === 401) {
-            router.push({ path: '/login' });
-          }
-          // Optionally handle other errors or propagate them
-        });
+    return axios.get(fullUrl, parameters)
+      .then(response => response.data)
+      .catch((error) => {
+        if (error.response.status === 401) {
+          router.push({
+            path: '/login'
+          });
+        }
+        // throw error
+      });
   }
 }
 
